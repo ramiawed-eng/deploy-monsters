27250930d8bf1d120e891fbb6ac1fcde875790b2
ramiawed-eng <ramiawed-eng@gmail.com>
2020-07-26 13:31:02 +0300

    Updates

---
+++ precache-manifest.eae696aa6e25dca26377d672f43ea8d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ef76ddae933f2ec64b61e298e644648",
+    "url": "/deploy-monsters/index.html"
+  },
+  {
+    "revision": "76663784d8b56d83e9d9",
+    "url": "/deploy-monsters/static/css/main.9b0e2a2b.chunk.css"
+  },
+  {
+    "revision": "5ba5ac096b0d3445fa8f",
+    "url": "/deploy-monsters/static/js/2.a8bb8d99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-monsters/static/js/2.a8bb8d99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76663784d8b56d83e9d9",
+    "url": "/deploy-monsters/static/js/main.9beb5aa5.chunk.js"
+  },
+  {
+    "revision": "9fb138396647009b8ae6",
+    "url": "/deploy-monsters/static/js/runtime-main.cd265240.js"
+  }
+]);
